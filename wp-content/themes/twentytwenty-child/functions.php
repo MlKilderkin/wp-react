2f3a9c5d317eaa9a2350c7e85fe0520c8699629a
MlKilderkin <mlkilderkin@gmail.com>
2023-05-13 08:14:43 +0300

    Disable admin bar for wp-test editor user

--- wp-content/themes/twentytwenty-child/functions.php
+++ wp-content/themes/twentytwenty-child/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', 'twenty_twenty_child_styles_enqueue' );
+add_action( 'after_setup_theme', 'twenty_twenty_child_hide_admin_bar' );
 
 function twenty_twenty_child_styles_enqueue() {
 	$parent_handle = 'twentytwenty-style';
@@ -17,4 +18,31 @@ function twenty_twenty_child_styles_enqueue() {
 		[ $parent_handle ],
 		$theme->get( 'Version' )
 	);
+}
+
+function twenty_twenty_child_hide_admin_bar() {
+	// Skip if it is admin, user not logged in or it is not an editor user
+	if ( is_admin() || ! is_user_logged_in() || ! is_editor_user() ) {
+		return;
+	}
+
+	global $current_user;
+
+	if ( $current_user->user_login !== 'wp-test' ) {
+		return;
+	}
+
+	show_admin_bar(false);
+}
+
+function is_editor_user() {
+	global $current_user;
+
+	foreach ( $current_user->roles as $role ) {
+		if ( $role === 'editor' ) {
+			return true;
+		}
+	}
+
+	return false;
 }
